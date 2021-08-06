2e5bbdb16c4d983e5b85f94e3504db8c601b6ed2
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2021-08-06 15:57:40 +0200

    Java mail configuration

--- src/main/java/org/javaemailclinet/Launcher.java
+++ src/main/java/org/javaemailclinet/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.javaemailclinet.view.ViewFactory;
 
+
 public class Launcher extends Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/javaemailclinet/controller/EmailLoginResult.java
@@ -0,0 +1,7 @@
+package org.javaemailclinet.controller;
+
+public enum EmailLoginResult {
+    SUCCESS,
+    FAILED_BY_CREDENTIALS,
+    FAILED_BY_UNEXPECTED_ERROR;
+}
