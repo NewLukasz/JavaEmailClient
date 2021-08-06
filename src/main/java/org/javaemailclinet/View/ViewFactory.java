9fbeb5c8a7ddb2291b347f6150c75ff6a8231d9f
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2021-08-06 08:14:06 +0200

    Add EmailManager, ViewFactory and AbstractController

---
+++ src/main/java/org/javaemailclinet/View/ViewFactory.java
@@ -0,0 +1,4 @@
+package org.javaemailclinet.View;
+
+public class ViewFactory {
+}
---
+++ src/main/java/org/javaemailclinet/controller/AbstractController.java
@@ -0,0 +1,11 @@
+package org.javaemailclinet.controller;
+
+import org.javaemailclinet.EmailManager;
+import org.javaemailclinet.View.ViewFactory;
+
+public abstract class AbstractController {
+
+    private EmailManager emailManager;
+    private ViewFactory viewFactory;
+
+}
