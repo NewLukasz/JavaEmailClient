16e088bf9723bafd8b297fa4b5166e573754e6e2
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2021-08-09 09:28:46 +0200

    Adding email maganer functionality

--- src/main/java/org/javaemailclinet/EmailManager.java
+++ src/main/java/org/javaemailclinet/EmailManager.java
@@ -1,4 +1,22 @@
 package org.javaemailclinet;
 
+import javafx.scene.control.TreeItem;
+import org.javaemailclinet.model.EmailAccount;
+
 public class EmailManager {
+    //Folder handling:
+    private TreeItem<String> foldersRoot = new TreeItem<String>("");
+    public TreeItem<String> getFoldersRoot(){
+        return foldersRoot;
+    };
+
+    public void addEmailAccount(EmailAccount emailAccount){
+        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
+        treeItem.setExpanded(true);
+        treeItem.getChildren().add(new TreeItem<String>("Inbox"));
+        treeItem.getChildren().add(new TreeItem<String>("Sent"));
+        treeItem.getChildren().add(new TreeItem<String>("Folder1"));
+        treeItem.getChildren().add(new TreeItem<String>("SPAM"));
+        foldersRoot.getChildren().add(treeItem);
+    }
 }
