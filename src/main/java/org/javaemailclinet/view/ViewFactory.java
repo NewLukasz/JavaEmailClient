0b7703445552b926f86ce8e46975188e543fd853
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2021-08-09 13:30:09 +0200

    Bugfix with show main window

--- src/main/java/org/javaemailclinet/view/ViewFactory.java
+++ src/main/java/org/javaemailclinet/view/ViewFactory.java
@@ -51,13 +51,13 @@ public class ViewFactory {
         System.out.println("Login windows - test");
         BaseController controller = new LoginWindowController(emailManager, this,"LoginWindow.fxml");
         initializeStage(controller);
-        mainViewInitialized = true;
     }
 
     public void showMainWindow(){
         System.out.println("Main windows - test");
         BaseController controller = new MainWindowController(emailManager, this,"MainWindow.fxml");
         initializeStage(controller);
+        mainViewInitialized = true;
     }
 
     public void showOptionWindow(){
