4f83b9d0c9ae8a14c9fce259843943eb53d75e17
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2021-08-11 11:22:55 +0200

    Bugfix with if statement

--- src/main/java/org/javaemailclinet/controller/services/FolderUpdaterService.java
+++ src/main/java/org/javaemailclinet/controller/services/FolderUpdaterService.java
@@ -23,8 +23,9 @@ public class FolderUpdaterService extends Service {
                     try{
                         Thread.sleep(5000);
                         for(Folder folder: folderList){
-                            if(folder.getType() != Folder.HOLDS_FOLDERS && folder.isOpen());
-                            folder.getMessageCount();
+                            if(folder.getType() != Folder.HOLDS_FOLDERS && folder.isOpen()){
+                                folder.getMessageCount();
+                            }
                         }
                     }catch (Exception e){
                         e.printStackTrace();
