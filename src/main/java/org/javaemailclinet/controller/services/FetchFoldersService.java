240e48bd255d38309ee96dabfda694dd778042d9
Lukasz Kilijanski <kilijanskilukasz@gmail.com>
2021-08-12 15:20:46 +0200

    Add icons for the rest of folders

--- src/main/java/org/javaemailclinet/controller/services/FetchFoldersService.java
+++ src/main/java/org/javaemailclinet/controller/services/FetchFoldersService.java
@@ -3,6 +3,7 @@ package org.javaemailclinet.controller.services;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import org.javaemailclinet.model.EmailTreeItem;
+import org.javaemailclinet.view.IconResolver;
 
 import javax.mail.Folder;
 import javax.mail.Message;
@@ -17,6 +18,7 @@ public class FetchFoldersService extends Service<Void> {
     private Store store;
     private EmailTreeItem<String> foldersRoot;
     private List<Folder> folderList;
+    private IconResolver iconResolver = new IconResolver();
 
     public FetchFoldersService(Store store, EmailTreeItem<String> foldersRoot, List<Folder> folderList) {
         this.store=store;
@@ -45,6 +47,7 @@ public class FetchFoldersService extends Service<Void> {
         for(Folder folder: folders){
             folderList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
+            emailTreeItem.setGraphic(iconResolver.getIconForFolder(folder.getName()));
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
             fetchMessagesOnFolder(folder, emailTreeItem);
