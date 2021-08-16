12bcdfc77490f8b54fdc87f83c0f2dfb8bf17136
NewLukasz <Kilijanskilukasz@gmail.com>
2021-08-16 18:36:09 +0200

    Add functionality to sending attachments

--- src/main/java/org/javaemailclinet/controller/ComposeMessageController.java
+++ src/main/java/org/javaemailclinet/controller/ComposeMessageController.java
@@ -6,17 +6,22 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.javaemailclinet.EmailManager;
 import org.javaemailclinet.controller.services.EmailSenderService;
 import org.javaemailclinet.model.EmailAccount;
 import org.javaemailclinet.view.ViewFactory;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ComposeMessageController extends BaseController implements Initializable {
 
+    private List<File> attachments = new ArrayList<File>();
 
     @FXML
     private TextField recipientTextField;
@@ -39,7 +44,8 @@ public class ComposeMessageController extends BaseController implements Initiali
                 emailAccountChoice.getValue(),
                 subjectTextField.getText(),
                 recipientTextField.getText(),
-                htmlEditor.getHtmlText()
+                htmlEditor.getHtmlText(),
+                attachments
         );
         emailSenderService.start();
         emailSenderService.setOnSucceeded(e->{
@@ -57,6 +63,15 @@ public class ComposeMessageController extends BaseController implements Initiali
                     break;
             }
         });
+    }
+
+    @FXML
+    void attachButtonAction(){
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if(selectedFile != null){
+            attachments.add(selectedFile);
+        }
     }
 
     public ComposeMessageController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
